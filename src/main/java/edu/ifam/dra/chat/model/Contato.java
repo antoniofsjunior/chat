3260438137f237d6e53283f24c99c23654a6126c
antoniofsjunior <ajunior.analista@gmail.com>
2023-08-21 18:27:57 -0400

    first commit

---
+++ src/main/java/edu/ifam/dra/chat/model/Contato.java
@@ -0,0 +1,56 @@
+package edu.ifam.dra.chat.model;
+
+public class Contato {
+	private String nome;
+	private String email;
+	private String telefone;
+	private String endereco;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+}
