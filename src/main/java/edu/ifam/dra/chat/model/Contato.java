cbcba124920609564b170601f12b6eb9c8a5d216
antoniofsjunior <ajunior.analista@gmail.com>
2023-09-04 18:29:28 -0400

    H2 + JPA + Contato Configuration

--- src/main/java/edu/ifam/dra/chat/model/Contato.java
+++ src/main/java/edu/ifam/dra/chat/model/Contato.java
@@ -1,5 +1,11 @@
 package edu.ifam.dra.chat.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Contato {
 	
 	/*
@@ -13,6 +19,11 @@ public class Contato {
         "estado" : "AM"
     }
 	 */
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String nome;
 	private String email;
 	private String telefone;
